55d6c4475bbb24d27a0498fd630d9ab6d5375772
jack-yan <yanzhangbao@163.com>
2017-08-30 09:45:19 +0800

    first commit

---
+++ src/main/java/com/lekusi/protobufdemo/Test.java
@@ -0,0 +1,7 @@
+package com.lekusi.protobufdemo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("he00llo");
+    }
+}
---
+++ src/main/java/com/lekusi/protobufdemo/TestProto.java
@@ -0,0 +1,104 @@
+package com.lekusi.protobufdemo;
+
+import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Protobuf 测试类
+ * 使用步骤：
+ * 1、编写*.proto文件
+ * 1、下载protoc-3.3.0-win32，
+ * 2、使用bin下的protoc.exe生成所需语言的实体类
+ *    cmd：protoc --java_out=[文件路径] [文件名]
+ * 3、将生成的实体类拷贝至项目中
+ * 4、添加protobuf-java-***.jar依赖包
+ * 5、使用如下代码进行读写测试
+ * <p>
+ * Created by Jack.Yan on 2017/8/8.
+ */
+public class TestProto {
+    public static void main(String[] args) {
+//        write2File("F:/", "Company.dat", createComay());//将对象数据写入文件
+        System.out.println(readFile("F:/","Company.dat"));//从文件读数据至对象
+    }
+
+    /**
+     * 将对象数据写入文件
+     * @param filePath 文件路径
+     * @param fileName 文件名
+     * @param bean 实体类
+     */
+    private static void write2File(String filePath,String fileName,GeneratedMessageV3 bean){
+        File file=new File(filePath,fileName);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bean.writeTo(fos);
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (IOException e1){
+            e1.getMessage();
+        }
+    }
+
+    /**
+     * 从文件读数据至对象
+     * @param filePath 文件路径
+     * @param fileName 文件名
+     * @return 实体类
+     */
+    private static CompanyProto.Compay readFile(String filePath, String fileName){
+        File file=new File(filePath,fileName);
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            return CompanyProto.Compay.parseFrom(fis);
+           /* int size=fis.available();
+            byte[] buffer=new byte[size];
+            fis.read(buffer);
+            return readBytes(buffer);*/
+        }catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch (IOException e1){
+            e1.getMessage();
+        }
+        return null;
+    }
+
+    /**
+     * 从byte数组读数据至对象
+     * @param data byte数组
+     * @return 实体类
+     */
+    private static CompanyProto.Compay readBytes(byte[] data){
+        try {
+            return CompanyProto.Compay.parseFrom(data);
+        } catch (InvalidProtocolBufferException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 创建实例化对象
+     * @return 实体类
+     */
+    private static CompanyProto.Compay createComay(){
+        return CompanyProto.Compay.newBuilder()
+                .setId(1)
+                .setName("琦彩科技")
+                .setAddress("环球中心S1-1213")
+                .addContact("028-66666660")
+                .addContact("028-66666661")
+                .setWebsite("www.ie-colorful.com")
+                .setType(CompanyProto.Compay.TYPE.PRIVATE)
+                .addStaffs(CompanyProto.Staff.newBuilder().setId(10001).setUserName("张三").setSex(CompanyProto.Staff.SEX.MALE).setAge(28).setDepartment("Dev").setJob("Android").addPhoneNums("15812345678").build())
+                .addStaffs(CompanyProto.Staff.newBuilder().setId(10001).setUserName("李四").setSex(CompanyProto.Staff.SEX.FEMALE).setAge(32).setDepartment("行政").setJob("人事").addPhoneNums("15899999999").addPhoneNums("15899999900").build())
+                .build();
+    }
+
+}
